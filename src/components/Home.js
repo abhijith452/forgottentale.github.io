8c39b3863fd5a75fb3f7698b3cc5055703e4f9a4
Abhijith Kannan <abhijithkannan452@gmail.com>
2020-05-09 15:05:31 +0530

    Home Page added

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,12 +5,17 @@ import './css/home.css'
 function Home() {
   return (
     <div className="Home">
-        <h1>Hi,<br />
-             I'm <span>Abhijith</span>,<br />
-             web developer</h1>
+        <div className="jumbotron">
+              <h1>Hi,<br />
+                I'm <span>Abhijith</span>,<br />
+                web developer</h1>
+             <p>Front-End Web Developer | IOT developer</p>
+             <button>See Projects</button>
+           </div>
              <svg xmlns="http://www.w3.org/2000/svg" width="800" height="785.671" viewBox="0 0 891.087 785.671">
-  <text id="_" data-name="&lt;&gt;" transform="matrix(0.914, -0.407, 0.407, 0.914, 0, 300.578)" fill="#efff00" font-size="390" font-family="OpenSans-SemiBold, Open Sans" font-weight="600" letter-spacing="0.138em"><tspan x="0" y="417">&lt;</tspan><tspan y="417" fill="#00f8ff">&gt;</tspan></text>
-</svg>
+             <text id="_" data-name="&lt;&gt;" transform="matrix(0.914, -0.407, 0.407, 0.914, 0, 300.578)" fill="#efff00" font-size="390" font-family="OpenSans-SemiBold, Open Sans" font-weight="600" letter-spacing="0.138em"><tspan x="0" y="417">&lt;</tspan><tspan y="417" fill="#00f8ff">&gt;</tspan></text>
+             </svg>
+
 
     </div>
   );
