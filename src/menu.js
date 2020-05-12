feedc02ec82a04ac5c7e18d63d6ac9ab0a4f34c8
Abhijith Kannan <abhijithkannan452@gmail.com>
2020-05-12 11:51:01 +0530

    adding responsiviness

--- src/menu.js
+++ src/menu.js
@@ -4,6 +4,11 @@ import {Link} from 'react-scroll'
 function Menu() {
   return (
     <div className="Menu">
+      <div className="hamburger">
+        <div className="toggle_div"></div>
+        <div className="toggle_div"></div>
+        <div className="toggle_div"></div>
+      </div>
       <ul>
           <li><Link to="Home" smooth={true} duration={800}><i className="fas fa-home fa-1x" ></i></Link></li>
           <li><Link to="Projects" smooth={true} duration={800}><i className="fas fa-project-diagram fa-1x" ></i></Link></li>
@@ -13,6 +18,7 @@ function Menu() {
           <li><Link to="Contact" smooth={true} duration={800}><i className="fas fa-envelope fa-1x" ></i></Link></li>
           
       </ul>
+      <div className="spacer"></div>
 
     </div>
   );
