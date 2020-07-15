a64127e2e32c96a9ec805d4a8a26481a4dabc4ae
Abhijith Kannan <abhijithkannan452@gmail.com>
2020-07-15 21:12:26 +0530

    Update Position.js

--- src/components/Position.js
+++ src/components/Position.js
@@ -11,13 +11,12 @@ function Position() {
    <Card 
      img = {msp}
      title = "Microsoft Student Partner"
-     des = "A realtime water pressure,ph and water flow rate monitoring system. A webapp is used to display the data on a chart"
+     des = "The Microsoft Student Partners is a worldwide recognizable program to sponsor students majoring in disciplines related to technology. As a MSP, i have hosted a small workshop on th topic 'Fundamentals of Azure ' at my college, hosted a webinar on the topic 'Getting started with Azure IoT' and have co organized two other webinars as well "
    />
    <Card 
      img = {bolt}
      title = "Bolt Student Ambassasor"
-     des = "A realtime water pressure,ph and water flow rate monitoring system. A webapp is used to display the data on a chart"
-   />
+     des = "The internship responsibilities includes promotion of Internet of Things and Machine Learning online training and organise a webinar on the topic 'IoT and ML meet-up' at my college" />
  
 
   
