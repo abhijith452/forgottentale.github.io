f298899c3cdabb74470e4d25e1ac2b77dcca8562
Abhijith Kannan <abhijithkannan452@gmail.com>
2020-07-17 20:13:19 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,17 +4,21 @@ import './scss/contact.scss';
 function Contact() {
   return (
     <div className="contact" id="Contact">
-<form name="contact" action="POST" netlify>
-  <p>
-    <label>Name <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Email <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+
+      <form className="contact__form" name="contact" method = "POST" netlify>
+
+        <h3 className="contact__form__title"><span>Contact Me</span></h3>
+
+        <div className="contact__form__container"></div>
+          <input className="contact__form__container__input" type="text" placeholder="Your Name" name="name"></input>
+          <input className="contact__form__container__input" type="email" placeholder="Your E-mail" name="email"></input>
+
+
+        <input type="text" className="contact__form__subject" placeholder="Subject" name="subject"></input>
+        <textarea placeholder="Message" name="message" className="contact__form__message"></textarea>
+        <button type="submit" className="contact__form__submit" >Sent</button>
+      </form >
+
 
 
       <div className="contact__socials">
