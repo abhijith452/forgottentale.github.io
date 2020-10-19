38131c52dd605ba40494da90d0fbceb3b6949074
ForgottenTale <abhijithkannan452@gmail.com>
2020-10-19 21:49:05 +0530

    clean plate

--- src/App.js
+++ src/App.js
@@ -1,44 +1,26 @@
-import React,{useEffect} from 'react';
-import Home from './components/Home';
-import Projects from './components/Project';
-import Positions from './components/Position';
-import Skills from './components/Skills';
-import Contact from './components/Contact';
-import Menu from './menu'
-import './components/scss/App.scss';
-import About from './components/About';
-
-
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-
-
-  
-
   return (
-    <div className="App" id="App">
-      
-    <Menu />
-    <Content/>
-   
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
 export default App;
-
-
-function Content() {
-  return (
-    <div className="Content">
-      <div className="wrapper">
-        <Home />
-        <Positions />
-        <Projects />
-        <Skills />
-        <About />
-        <Contact />
-      </div>
-    </div>
-  );
-}
