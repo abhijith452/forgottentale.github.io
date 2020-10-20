28d7179f0f7d2d00cc0aa492e33830bd5f27090c
ForgottenTale <abhijithkannan452@gmail.com>
2020-10-20 11:41:49 +0530

    Project Page is added

---
+++ src/components/projects/projects.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './projects.scss';
+
+function Projects(){
+    return(
+        <div className="projects">
+            <h1 className="projects__title">WDNMS</h1>
+            <p className="projects__subTitle">Water Distribution Network Monitoring System</p>
+            </div>
+    );
+ 
+}
+
+export default Projects;
